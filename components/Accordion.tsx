056b36840cddefe21f893e9d55f5c30eaf21541e
Quynh Nguyen <tom@coh.dev>
2022-12-17 18:41:45 +0700

    pudate

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -5,7 +5,7 @@ const Accordion = ({ title, content }:{title: any, content: any}) => {
 
   return (
     <div className="accordion-item">
-      <div className="accordion-title p-5 border-solid border-2 border-black -mt-0.5 text-left" onClick={() => setIsActive(!isActive)}>
+      <div className="accordion-title p-2 border-solid border-2 border-black -mt-0.5 text-left text-sm font-semibold" onClick={() => setIsActive(!isActive)}>
         <div><span>{isActive ? 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline">
         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -16,7 +16,7 @@ const Accordion = ({ title, content }:{title: any, content: any}) => {
 </svg>
 }</span><span className='ml-3'>{title}</span></div>
       </div>
-      {isActive && <div className="accordion-content p-5 -mt-0.5 border-solid border-2 border-black">{content}</div>}
+      {isActive && <div className="accordion-content p-5 -mt-0.5 border-solid border-2 border-black text-sm">{content}</div>}
     </div>
   );
 };
