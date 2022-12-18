09ea0c30784dee692b6082920aa75415e093701e
Quynh Nguyen <tom@coh.dev>
2022-12-18 14:24:03 +0700

    add locale

--- pages/about.tsx
+++ pages/about.tsx
@@ -52,9 +52,13 @@ const Home: NextPage = () => {
       <div className="max-w-[1240px] mx-auto p-4 text-left">
         <div className="grid grid-rows-none md:grid-cols-2 p-4 gap-4">
           <div className="w-full h-full col-span-2 md:col-span-1 row-span-2 pt-10">
-            <p>Hệ thống y tế ECHO MEDI bổ sung cho những phương pháp chăm sóc sức khoẻ truyền thống hiện nay thông qua bốn nền tảng: chăm sóc phòng ngừa, điều trị ban đầu, quản lý bệnh mạn tính và sức khoẻ toàn diện.</p>
-            <p>Chúng tôi đặc biệt đề cao việc phòng ngừa và tối ưu chất lượng cuộc sống cho các thành viên, góp phần thay đổi tư duy và áp dụng lối sống lành mạnh để ngăn ngừa bệnh tật, cải thiện sức khoẻ thể chất và tinh thần, hướng tới một sức khoẻ toàn diện.</p>
-            <p>ECHO MEDI tự hào với đội ngũ chuyên gia, bác sĩ, dược sĩ và điều dưỡng được đào tạo và làm việc tại các đơn vị hàng đầu trong và ngoài nước: Đại Học Y Khoa Wisconsin (Hoa Kỳ), Đại Học Y Dược TP Hồ Chí Minh, Đại Học Y Khoa Phạm Ngọc Thạch,… Bệnh Viện USC (Hoa Kỳ), Bệnh Viện Chợ Rẫy, Bệnh Viện Thống Nhất, Bệnh Viện Nhiệt Đới, Bệnh Viện 115…</p>
+            <p className="mb-4">
+              {locale === "en" ? "ECHO MEDI’s healthcare system supplements to the current healthcare crisis management." : "Hệ thống y tế ECHO MEDI bổ sung cho những phương pháp chăm sóc sức khoẻ truyền thống hiện nay thông qua bốn nền tảng: chăm sóc phòng ngừa, điều trị ban đầu, quản lý bệnh mạn tính và sức khoẻ toàn diện."}</p>
+            <p className="mb-4">
+              {locale === "en" ? "ECHO MEDI’s four pillars of healthcare system is to utilize a pathogenic and reactive approach, focusing on causes, consequences, diagnosis and treatment of diseases and injuries. We focus proactively on prevention as well as maximizing our members vitality, adopting attitudes and lifestyles that prevent disease, improve health, and enhance their quality of life and sense of wellbeing." : "Chúng tôi đặc biệt đề cao việc phòng ngừa và tối ưu chất lượng cuộc sống cho các thành viên, góp phần thay đổi tư duy và áp dụng lối sống lành mạnh để ngăn ngừa bệnh tật, cải thiện sức khoẻ thể chất và tinh thần, hướng tới một sức khoẻ toàn diện."}</p>
+            <p className="mb-4">
+              {locale === "en" ? "Besides, ECHO MEDI is proud to have a team of experts, doctors, pharmacists and nurses coming from leading domestic and foreign training institutions, for example, Milwaukee-Wisconsin Medical University, Ho Chi Minh City University of Medicine and Pharmacy, Pham Ngoc Thach Medical University, Ton Duc Thang University." :
+              "ECHO MEDI tự hào với đội ngũ chuyên gia, bác sĩ, dược sĩ và điều dưỡng được đào tạo và làm việc tại các đơn vị hàng đầu trong và ngoài nước: Đại Học Y Khoa Wisconsin (Hoa Kỳ), Đại Học Y Dược TP Hồ Chí Minh, Đại Học Y Khoa Phạm Ngọc Thạch,… Bệnh Viện USC (Hoa Kỳ), Bệnh Viện Chợ Rẫy, Bệnh Viện Thống Nhất, Bệnh Viện Nhiệt Đới, Bệnh Viện 115…"}</p>
           </div>
           <div className="w-full h-full col-span-2 md:col-span-1 row-span-2 pt-10">
             <div className="flex justify-center">
