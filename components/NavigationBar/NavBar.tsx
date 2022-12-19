ca87f6715c79719fcea1d6d82da31858eefd88ea
Quynh Nguyen <tom@coh.dev>
2022-12-19 08:58:26 +0700

    update

--- components/NavigationBar/NavBar.tsx
+++ components/NavigationBar/NavBar.tsx
@@ -484,55 +484,63 @@ const NavBar = () => {
           </li>
         </ul>
         <div className="flex space-x-2 justify-center">
-                  <a href={(locale === "en" ? "/en" : "") + "/contact"} type="button" className="font-semibold ml-5 sm:ml-10 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out bg-green-700">
-                    {locale === "en" ? "Contact" : "Liên hệ"}
-                  </a>
-                </div>
-        <div className="m-auto">
-        <div className="relative group hidden sm:block">
-          <button className="text-black h-full flex flex-row items-center w-full text-base font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 focus:outline-none font-montserrat">
-            {locale === "en" ? <div
-            className="flex"><Image
-              src={IgImg3}
-              alt="/"
-              width={25}
-              height={25}
-            /><span className="ml-2">EN</span></div> : 
-            <div
-            className="flex"><Image
-              src={IgImg4}
-              alt="/"
-              width={25}
-              height={25}
-            /><span className="ml-2">VI</span></div>
-            }
-          </button>
-          <div className="absolute z-10 hidden bg-grey-200 group-hover:block bg-emgreen">
-            <div className="bg-regal-blue text-black bg-white font-semibold" style={{width: "100px"}}>
-              <div className="flex">
+          <a href={(locale === "en" ? "/en" : "") + "/contact"} type="button" className="font-semibold ml-5 sm:ml-2 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out bg-green-700">
+            {locale === "en" ? "Contact" : "Liên hệ"}
+          </a>
+        </div>
+        <div className="flex space-x-2 justify-center">
+          <a href={(locale === "en" ? "/en" : "") + "/cart"} type="button" className="text-green-800 ml-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+            </svg>
 
-                <button onClick={() => {
-                  router.push(router.asPath, router.asPath, { locale: locale === "en" ? "vi" : "en" });
-                }}>
-                {locale === "en" ? <div
-            className="flex"><Image
-              src={IgImg4}
-              alt="/"
-              width={25}
-              height={25}
-            /><span className="ml-2">VI</span></div> : 
-            <div
-            className="flex"><Image
-              src={IgImg3}
-              alt="/"
-              width={25}
-              height={25}
-            /><span className="ml-2">EN</span></div>}
-                </button>
+          </a>
+        </div>
+        <div className="m-auto">
+          <div className="relative group hidden sm:block">
+            <button className="text-black h-full flex flex-row items-center w-full text-base font-semibold text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 focus:outline-none font-montserrat">
+              {locale === "en" ? <div
+                className="flex"><Image
+                  src={IgImg3}
+                  alt="/"
+                  width={25}
+                  height={25}
+                /><span className="ml-2">EN</span></div> :
+                <div
+                  className="flex"><Image
+                    src={IgImg4}
+                    alt="/"
+                    width={25}
+                    height={25}
+                  /><span className="ml-2">VI</span></div>
+              }
+            </button>
+            <div className="absolute z-10 hidden bg-grey-200 group-hover:block bg-emgreen">
+              <div className="bg-regal-blue text-black bg-white font-semibold" style={{ width: "100px" }}>
+                <div className="flex">
+
+                  <button onClick={() => {
+                    router.push(router.asPath, router.asPath, { locale: locale === "en" ? "vi" : "en" });
+                  }}>
+                    {locale === "en" ? <div
+                      className="flex"><Image
+                        src={IgImg4}
+                        alt="/"
+                        width={25}
+                        height={25}
+                      /><span className="ml-2">VI</span></div> :
+                      <div
+                        className="flex"><Image
+                          src={IgImg3}
+                          alt="/"
+                          width={25}
+                          height={25}
+                        /><span className="ml-2">EN</span></div>}
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
         {/* Mobile Button */}
         <div onClick={handleNav} className="block sm:hidden z-10">
