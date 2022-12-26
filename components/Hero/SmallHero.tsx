b2b20972db793a3b670218c989822be753aa690b
Quynh Nguyen <tom@coh.dev>
2022-12-26 15:05:55 +0700

    update

--- components/Hero/SmallHero.tsx
+++ components/Hero/SmallHero.tsx
@@ -25,7 +25,7 @@ const SmallHero = ({ heading, message, image_url, sub_message, image_placeholder
   const loaded = useProgressiveImage(image_url)
 
   return (
-    <div className="flex items-center justify-center h-[200px] mt-[100px] bg-fixed bg-center bg-cover"
+    <div className="flex items-center justify-center h-[300px] mt-[100px] bg-fixed bg-center bg-cover"
       // style={{backgroundImage: "url(" + image_url + ")"}}
       style={{backgroundImage: `url(${loaded ||  image_placeholder_url})`}}
     >
@@ -35,7 +35,7 @@ const SmallHero = ({ heading, message, image_url, sub_message, image_placeholder
               src={image_url} // use normal <img> attributes as props
               placeholderSrc={image_url}
                /> */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] h-[200px] mt-[100px]" />
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] h-[300px] mt-[100px]" />
       <div className="p-5 text-white z-[2] max-w-[1048px]">
         <h2 className="text-5xl font-semibold">{heading}</h2>
         <p className="py-5 text-xl sm:text-lg">{message}</p>
--- pages/[locale]/services/[slug].tsx
+++ pages/[locale]/services/[slug].tsx
@@ -22,6 +22,7 @@ import remarkGfm from 'remark-gfm'
 import remarkBreaks from 'remark-breaks';
 
 import { makeStaticProps, getStaticPathsServices, getStaticPropsService } from '../../../lib/getStatic';
+import SmallHero from '../../../components/Hero/SmallHero';
 
 // const getStaticProps = makeStaticProps(['common', 'footer']);
 export { getStaticPathsServices as getStaticPaths, getStaticPropsService as getStaticProps };
@@ -132,7 +133,7 @@ const Blog = (props: InferGetStaticPropsType<typeof getStaticPropsService>) => {
         />
         <link rel="icon" href="/favicon1.png" />
       </Head>
-      <Hero heading={locale === "en" ? props.en_label : props.label} message={locale === "en" ? props.en_desc : props.desc} 
+      <SmallHero heading={locale === "en" ? props.en_label : props.label} message={locale === "en" ? props.en_desc : props.desc} 
         image_url={"http://3.89.245.84:1337" + props.image_url} 
         image_placeholder_url={"http://3.89.245.84:1337" + props.placeholder_image_url} />
       <div className="max-w-[1048px] mx-auto py-16 text-left">
