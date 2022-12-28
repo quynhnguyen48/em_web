1adbdf2b7be62d493eb372c110c36025413352ba
Quynh Nguyen <tom@coh.dev>
2022-12-28 11:10:40 +0700

    update

--- pages/[locale]/gift.tsx
+++ pages/[locale]/gift.tsx
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
     const [email, setEmail] = useState("");
     const [phone_number, setPhoneNumber] = useState("");
     const [message, setMessage] = useState("");
-    const [active, setActive] = useState(-1);
+    const [option, setOption] = useState("1");
     const router = useRouter()
     const locale = router.query.locale as string || 'en';
 
@@ -269,46 +269,111 @@ const Home: NextPage = () => {
                                         placeholder={locale === "en" ? "Your address" : "Địa chỉ của bạn"}
                                         onChange={(e) => { setName(e.target.value) }}
                                     />
+                                    <p className="font-semibold mt-5">
+                                        {locale == "en" ? "Choose your packages" : "Chọn gói khám phù hợp với người nhận"}</p>
+                                    <div className="form-control
+                    mb-3
+                    block
+                    w-full
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                <select 
+                onChange={e => {
+                    setOption(e.target.value);
+                }}
+                className="px-3 w-full
+                    py-1.5">
+                  <option value={1}>For Dad/ Grandpa</option>
+                  <option value={2}>For Mom/ Grandma</option>
+                  <option value={3}>For Siblings/ Relatives</option>
+                  <option value={4}>For Corporate/ Employees</option>
+                  <option value={5}>For Membership</option>
+                </select>
+              </div>
                                     <p className="font-semibold">{locale == "en" ? "Choose your Gifting packages" : "Chọn gói khám muốn dành tặng"}</p>
-                                    <div>
+                                    {(option == "1" || option == "2" || option == "3" || option == "4") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>
                                             {locale == "en" ? "General Healthcare" :"Gói Khám Sức Khỏe Tổng Quát"}
                                         </span>
-                                    </div>
-                                    <div>
+                                    </div>}
+                                    {(option == "1" || option == "3") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>
-                                            {locale == "en" ? " Men's Health Care" :"Gói Khám Sức Khỏe Tổng Quát Dành Cho Nam Giới"}
+                                            {locale == "en" ? "Men's Health Care" :"Gói Khám Sức Khỏe Tổng Quát Dành Cho Nam Giới"}
                                             </span>
-                                    </div>
-                                    <div>
+                                    </div>}
+                                    {(option == "2" || option == "3") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>
+                                            {locale == "en" ? "Women's Health Care" :" Gói Khám Sức Khỏe Tổng Quát Dành Cho Phụ Nữ"}
+                                            </span>
+                                    </div>}
+                                    {(option == "1" || option == "2") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
                                             {locale == "en" ? "Hypertension Management" :"Gói Quản Lý Ngoại Trú Bệnh Tăng Huyết Áp"}
                                             </span>
-                                    </div>
-                                    <div>
+                                    </div>}
+                                    {(option == "1" || option == "2") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
-                                            {locale == "en" ? " Diabetes" :"Gói Quản Lý Ngoại Trú Bệnh Đái Tháo Đường"}
+                                            {locale == "en" ? "Diabetes" :"Gói Quản Lý Ngoại Trú Bệnh Đái Tháo Đường"}
                                             </span>
-                                    </div>
+                                    </div>}
+                                    {(option == "3") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
+                                            {locale == "en" ? "Reproductive Health Screening" :" Tầm Soát Sức Khỏe Sinh Sản"}
+                                            </span>
+                                    </div>}
+                                    {(option == "4") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
+                                            {locale == "en" ? "Post COVID-19" :"Gói Khám Sức Khỏe Hậu COVID-19"}
+                                            </span>
+                                    </div>}
+                                    {(option == "4") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
+                                            {locale == "en" ? "Musculoskeletal Screening" :"Tầm Soát Bệnh Lý Cơ Xương Khớp"}
+                                            </span>
+                                    </div>}
                                     <p className="font-semibold mt-5">
                                         {locale == "en" ? "Add-on package" : "Gói khám bổ sung"}</p>
-                                    <div>
+                                    {(option == "1" || option == "4") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
                                            {locale =="en" ? " Smoking Cessation Program" : "Chương Trình Cai Thuốc Lá"}</span>
-                                    </div>
-                                    <div>
+                                    </div>}
+                                    {(option == "2" || option == "4") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
-                                            {locale == "en" ? " Weight Management" : "Quản Lý Cân Nặng"}</span>
-                                    </div>
-                                    <div>
+                                           {locale =="en" ? "Mental Wellbeing" : "Sức Khỏe Tinh Thần"}</span>
+                                    </div>}
+                                    {(option == "1" || option == "2" || option == "3" || option == "4") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span> 
+                                            {locale == "en" ? "Weight Management" : "Quản Lý Cân Nặng"}</span>
+                                    </div>}
+                                    {(option == "1") && <div>
                                         <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>  
-                                        {locale == "en" ? "  Heart Health Program" : "Chương Trình Sức Khỏe Tim Mạch"}</span>
-                                    </div>
+                                        {locale == "en" ? "Heart Health Program" : "Chương Trình Sức Khỏe Tim Mạch"}</span>
+                                    </div>}
+                                    {(option == "2") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>  
+                                        {locale == "en" ? "Hormonal Health" : "Sức Khỏe Nội Tiết Tố"}</span>
+                                    </div>}
+                                    {(option == "3") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>  
+                                        {locale == "en" ? "Metabolism And Heart Health" : "Khả Năng Trao Đổi Chất Và Sức Khỏe Tim Mạch"}</span>
+                                    </div>}
+                                    {(option == "3") && <div>
+                                        <input type="checkbox" className="mr-2 inline border-gray-300 rounded h-4 w-4" /><span>  
+                                        {locale == "en" ? "Skincare And Anti-aging Therapy" : "Chăm Sóc Da Và Ngăn Ngừa Lão Hóa"}</span>
+                                    </div>}
                                     <div className="mt-5">
                                         <p>{locale == "en" ? "What message would you like to include?" : "Thông điệp bạn muốn nhắn nhủ đến người nhận?"}</p>
                                         <textarea className="w-full h-32" />
                                     </div>
-                                    <div className="mt-10 mb-4 inline-block px-6 py-2.5 text-white font-semibold text-sm leading-tight uppercase rounded-full shadow-md hover:bg-green-300 hover:shadow-lg focus:bg-green-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-300 active:shadow-lg transition duration-150 ease-in-out bg-green-200 text-black rounded-full">
+                                    <div className="mt-10 mb-4 inline-block px-6 py-2.5 text-white font-semibold text-sm leading-tight uppercase rounded shadow-md hover:bg-emgreen hover:shadow-lg focus:bg-emgreen focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-300 active:shadow-lg transition duration-150 ease-in-out bg-emgreen text-white rounded">
                                         <LinkComponent href={"/insight"} locale="" skipLocaleHandling={false}>
                                             {locale == "en" ? "Send gift" : "Gửi quà"}</LinkComponent>
                                     </div>
