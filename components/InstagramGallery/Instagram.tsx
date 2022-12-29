afea7f812a00352ae593874532a594fbcb52070d
Quynh Nguyen <tom@coh.dev>
2022-12-29 15:54:15 +0700

    update

--- components/InstagramGallery/Instagram.tsx
+++ components/InstagramGallery/Instagram.tsx
@@ -17,7 +17,7 @@ const Instagram = () => {
         "ECHO MEDI mang đến cho các thành viên dịch vụ chăm sóc sức khoẻ đồng bộ và toàn diện mọi lúc, mọi nơi."
         }
       </p>
-      <p className="pb-4 text-white text-lg text-left">{
+      <p className="text-white text-lg text-left">{
       locale === "en" ? "ECHO MEDI members have immediate access to our healthcare professionals and wellness care team, allowing us to deliver the ultimate wellbeing to members whom we know personally and to whom we hold ourselves accountable to every single member, so members can rest assure to entrust their health to us." :     
       "Các thành viên của ECHO MEDI được theo sát và thiết kế dịch vụ chăm sóc sức khoẻ tổng quát tối ưu bởi đội ngũ tư vấn sức khoẻ cùng các chuyên gia y tế. Chính vì thế, bạn có thể yên tâm uỷ thác sức khoẻ của mình cho chúng tôi"}.</p>
       </div>
